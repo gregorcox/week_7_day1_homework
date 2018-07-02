1f99a27a48b6d6171aebfe4e0e502613455c0a52
Gregor Cox <gregorcox@gmail.com>
2018-07-02 16:41:12 +0100

    implements ISecurity method

--- src/main/java/Rollercoaster.java
+++ src/main/java/Rollercoaster.java
@@ -1,6 +1,14 @@
-public class Rollercoaster extends Attraction {
+public class Rollercoaster extends Attraction implements ISecurity {
 
     public Rollercoaster(String name){
         super(name);
+    }
+
+    public boolean isAllowedTo(Visitor visitor){
+        if (visitor.getAge() >= 12 && visitor.getHeight() > 145) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/TobaccoStall.java
+++ src/main/java/TobaccoStall.java
@@ -1,7 +1,15 @@
-public class TobaccoStall extends Stall {
+public class TobaccoStall extends Stall implements ISecurity {
 
     public TobaccoStall(String name, String ownerName, int parkingSpot){
         super(name, ownerName, parkingSpot);
 
+    }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() > 18) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
